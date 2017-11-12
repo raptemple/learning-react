7a4b125ef5a665fbd512d730bc50e047eeafc3f6
lgieco <lauragieco@gmail.com>
2017-11-13 09:58:36 +1000

    update component state

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      }, {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      }, {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: this.state.contacts.filter(c => c.id !== contact.id)
+    }))
+  }
+
   render() {
-    return (
-        <ListContacts contacts={contacts} />
-    )
+    return (<div>
+      <ListContacts contacts={this.state.contacts} onDeleteContact={this.removeContact}/>
+    </div>)
   }
 }
-
 export default App;

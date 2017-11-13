5b488f2fce8949ebaaf59c00b7e6ba340dccc59b
lgieco <lauragieco@gmail.com>
2017-11-13 10:20:21 +1000

    Validate proptypes

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
-import React /*, {Component}*/ from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 // Stateless Functional Component
 function ListContacts(props) {
@@ -20,25 +21,9 @@ function ListContacts(props) {
   </ol>)
 }
 
-// class ListContacts extends Component {
-//   render() {
-//     return (<ol className='contact-list'>
-//       {
-//         this.props.contacts.map(contact => (<li key={contact.id} className='contact-list-item'>
-//           <div className='contact-avatar' style={{
-//               backgroundImage: `url(${contact.avatarURL})`
-//             }}/>
-//           <div className='contact-details'>
-//             <p>{contact.name}</p>
-//             <p>{contact.email}</p>
-//           </div>
-//           <button className='contact-remove'>
-//             Remove
-//           </button>
-//         </li>))
-//       }
-//     </ol>)
-//   }
-// }
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired
+}
 
 export default ListContacts

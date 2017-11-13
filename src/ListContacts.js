a439765420b05c6e8dfca53af8f46ff0e4bc586e
lgieco <lauragieco@gmail.com>
2017-11-13 10:49:30 +1000

    object destructuring

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -19,12 +19,15 @@ class ListContacts extends Component {
   }
 
   render() {
+    const { contacts, onDeleteContact } = this.props
+    const { query } = this.state
+
     let showingContacts
-    if (this.state.query) {
+    if (query) {
       const match = new RegExp(escapeRegExp(this.state.query), 'i')
-      showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+      showingContacts = contacts.filter((contact) => match.test(contact.name))
     } else {
-      showingContacts = this.props.contacts
+      showingContacts = contacts
     }
 
     showingContacts.sort(sortBy('name'))
@@ -34,7 +37,7 @@ class ListContacts extends Component {
         <input
           className='search-contacts'
           type='text' placeHolder='Search contacts'
-          value={this.state.query}
+          value={query}
           onChange={(event) => this.updateQuery(event.target.value)}/>
       </div>
       <ol className='contact-list'>
@@ -47,7 +50,7 @@ class ListContacts extends Component {
               <p>{contact.name}</p>
               <p>{contact.email}</p>
             </div>
-            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+            <button onClick={() => onDeleteContact(contact)} className='contact-remove'>
               Remove
             </button>
           </li>))

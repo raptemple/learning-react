2e15fa329ec5e7ba86f6eeb677d4f60877350c8c
lgieco <lauragieco@gmail.com>
2017-11-15 19:51:28 +1000

    form and routing

--- src/App.js
+++ src/App.js
@@ -23,6 +23,15 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then(contact => {
+        this.setState(state => ({
+          contacts: state.contacts.concat([contact])
+        }))
+      })
+  }
+
   render() {
     return (<div className='app'>
       <Route exact path='/' render={() => (
@@ -30,7 +39,13 @@ class App extends Component {
           onDeleteContact={this.removeContact}
           />
         )}/>
-      <Route path='/create' component={CreateContact}/>
+      <Route path='/create' render={({history}) => (
+        <CreateContact onCreateContact={(contact) => {
+          this.createContact(contact)
+          history.push('/')
+        }}
+        />
+      )}/>
     </div>)
   }
 }
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import ImageInput from './ImageInput'
+import serializeForm from 'form-serialize'
+
 
 class CreateContact extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const values = serializeForm(event.target, { hash: true })
+    if (this.props.onCreateContact)
+      this.props.onCreateContact(values)
+  }
+
   render() {
-    return (<div>Hello I'm new</div>)
+    return (<div>
+      <Link className='close-create-contact' to='/'>Close</Link>
+      <form onSubmit={ this.handleSubmit } className='create-contact-form'>
+        <ImageInput
+          className='create-contact-avatar-input'
+          name='avatarURL'
+          maxHeight={64}
+          />
+          <div className='create-contact-details'>
+            <input type='text' name='name' placeholder='Name'/>
+            <input type='text' name='email' placeholder='Email'/>
+            <button>Add contact</button>
+          </div>
+      </form>
+
+      </div>)
   }
 }
 
